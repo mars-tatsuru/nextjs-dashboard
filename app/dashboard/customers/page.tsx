424c8c02312b598e6f5cd70ce28b477328b4db83
tatsuru koike <t-koike@marsflag.com>
2024-01-16 18:26:31 +0900

    feature: add accessibility and meta data

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from 'next';
+import Table from '@/app/ui/customers/table';
+
+export const metadata: Metadata = {
+  title: 'Customer',
+};
+
 export default function Page() {
   return <p>Customers Page</p>;
 }
